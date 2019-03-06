1e77f991e526677a9cc67e55d829e35394f2f7cb
Michelle Oliveira <miolivc@outlook.com>
2019-03-06 17:53:06 -0300

    Corrigindo problemas de ordenação de datatable de acordo com os atributos

--- src/main/java/br/edu/ifpb/cultural/booking/controller/AtracaoController.java
+++ src/main/java/br/edu/ifpb/cultural/booking/controller/AtracaoController.java
@@ -3,21 +3,33 @@ package br.edu.ifpb.cultural.booking.controller;
 import br.edu.ifpb.cultural.booking.domain.Atracao;
 import br.edu.ifpb.cultural.booking.infra.model.AtracaoRepository;
 
-import javax.faces.view.ViewScoped;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
 import java.util.List;
 
 @Named
-@ViewScoped
+@RequestScoped
 public class AtracaoController implements Serializable {
 
     @Inject
-    private AtracaoRepository atracoes;
+    private AtracaoRepository atracaoRepository;
+    private List<Atracao> atracoes;
 
-    public List<Atracao> getTodasAsAtracoes() {
-        return atracoes.getAll();
+    @PostConstruct
+    public void init() {
+        this.atracoes = atracaoRepository.getAll();
+    }
+
+//    Getters and Setters
+    public List<Atracao> getAtracoes() {
+        return atracoes;
+    }
+
+    public void setAtracoes(List<Atracao> atracoes) {
+        this.atracoes = atracoes;
     }
 
 }
